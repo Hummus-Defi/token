685d4e165607bf1a65522de9c8104ed2d73add8b
OdysseusTheKing <odysseus.the.king@protonmail.com>
2022-03-29 23:18:39 -0700

    Andromeda Deployment

--- scripts/deploy_token.ts
+++ scripts/deploy_token.ts
@@ -1,12 +1,12 @@
 import { ethers, getNamedAccounts } from 'hardhat'
 
-const MINTING_TIMESTAMP = 1681948800 // 2022/04/20 12:00 AM GMT
+const MINTING_TIMESTAMP = 1681948800 // 2023/04/20 12:00 AM GMT
 
 async function main() {
-  const { deployer } = await getNamedAccounts()
+  const { deployer, multisig } = await getNamedAccounts()
   const TokenFactory = await ethers.getContractFactory('Hum')
   console.log('Deploying Token...')
-  const token = await TokenFactory.deploy(deployer, deployer, MINTING_TIMESTAMP)
+  const token = await TokenFactory.deploy(deployer, multisig, MINTING_TIMESTAMP)
   await token.deployed()
   console.log('Deployed to:', token.address)
 }
