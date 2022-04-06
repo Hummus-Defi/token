1b2d781daba6a20f2eb0357d520755beb91c3cd3
OdysseusTheKing <odysseus.the.king@protonmail.com>
2022-04-05 20:34:09 -0700

    Andromeda Staking USD Setup

--- scripts/setup_staking.ts
+++ scripts/setup_staking.ts
@@ -1,9 +1,9 @@
 import { parseEther } from 'ethers/lib/utils'
 import { ethers, getNamedAccounts } from 'hardhat'
 
-const HUM_PER_SEC = parseEther('1') //'913242009132420000'
+const HUM_PER_SEC = '4629629629629629629' // parseEther('1')
 const DIALUTION = 375
-const START_TIMESTAMP = 0
+const START_TIMESTAMP = 1649890800 // 2022-04-13 11:00PM GMT (4:00PM PST)
 
 async function main() {
   const { STAKING, TOKEN, ESCROW } = await getNamedAccounts()
