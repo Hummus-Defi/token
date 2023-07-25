ed5feef6e7b779ca3e058ba35d0048fdc91d1117
OdysseusTheKing <geomagnetized@gmail.com>
2023-07-24 19:00:29 -0700

    Update Bribe Script

--- tasks/bribe.ts
+++ tasks/bribe.ts
@@ -4,17 +4,16 @@ import { task, types } from 'hardhat/config'
 task('deploy-bribe', 'Deploy a Bribe')
   .addParam('lp', 'The address of the LP Token', ethers.constants.AddressZero, types.string)
   .addOptionalParam('reward', 'The address of the reward token, only if non-native', undefined, types.string)
-  .addOptionalParam('tokenPerSec', 'The amount of tokens to emit per second', '0', types.string)
-  .setAction(async ({ lp, reward, tokenPerSec }, { ethers, getNamedAccounts, run }) => {
+  .addOptionalParam('rate', 'The amount of tokens to emit per second', '0', types.string)
+  .setAction(async ({ lp, reward, rate }, { ethers, getNamedAccounts, run }) => {
     const { METIS, VOTER } = await getNamedAccounts()
 
     const factory = await ethers.getContractFactory('Bribe')
-    const bribe = await factory.deploy(VOTER, lp, reward ?? METIS, tokenPerSec, reward ? false : true)
+    const bribe = await factory.deploy(VOTER, lp, reward ?? METIS, rate, reward ? false : true)
     await bribe.deployed()
-
 
     await run('verify:verify', {
       address: bribe.address,
-      constructorArguments: [VOTER, lp, reward ?? METIS, tokenPerSec, reward ? false : true],
+      constructorArguments: [VOTER, lp, reward ?? METIS, rate, reward ? false : true],
     })
   })
